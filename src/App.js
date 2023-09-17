7e524203bca09117265bbb8ee4675e0a77e2e89a
ijidakinroade <33587213+ijidakinroade@users.noreply.github.com>
2023-09-17 12:15:07 -0400

    Added baby gif and added background color black

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className="Dancing_Baby">
+            <img src={process.env.PUBLIC_URL + '/very_scary.gif'} alt="Dancing Baby" />
+          </div>
+
+
     </div>
   );
 }
